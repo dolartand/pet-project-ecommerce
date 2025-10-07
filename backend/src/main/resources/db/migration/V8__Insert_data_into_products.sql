e0aab922cf4049f81efb69c58ed2e807e5d39af1
Dasha Pimoshenko <pimadasha2006@gmail.com>
2025-10-07 15:10:24 +0300

    refactor: Changed links to images in db

--- backend/src/main/resources/db/migration/V8__Insert_data_into_products.sql
+++ backend/src/main/resources/db/migration/V8__Insert_data_into_products.sql
@@ -1,9 +1,9 @@
 INSERT INTO products (name, description, price, category_id, image_url, available, rating)
-VALUES ('Смартфон Alpha', 'Последняя модель с улучшенной камерой', 999.99, 3, 'http://example.com/images/alpha.jpg',
+VALUES ('Смартфон Alpha', 'Последняя модель с улучшенной камерой', 999.99, 3, 'https://www.ixbt.com/img/n1/news/2025/0/5/7895cdf7-492e-4565-861a-7fba70d6ae59_large.jpg',
         TRUE, 4.5),
-       ('Смартфон Beta', 'Бюджетная модель с большим экраном', 499.50, 3, 'http://example.com/images/beta.jpg', TRUE,
+       ('Смартфон Beta', 'Бюджетная модель с большим экраном', 499.50, 3, 'https://main-cdn.sbermegamarket.ru/upload/smartfon-Honor_X8a-2_66951a788c5e0.jpg', TRUE,
         4.1),
-       ('Книга "Основы SQL"', 'Полное руководство для начинающих', 29.99, 2, 'http://example.com/images/sql-book.jpg',
+       ('Книга "Основы SQL"', 'Полное руководство для начинающих', 29.99, 2, 'https://content.img-gorod.ru/pim/products/images/6f/0f/018ee0c1-cb86-72ca-be63-928004a76f0f.jpg?width=3056&height=1476&fit=bounds',
         TRUE, 4.8),
        ('Книга "Изучаем Python"', 'Практическое пособие по Python', 35.00, 2,
-        'http://example.com/images/python-book.jpg', FALSE, 4.7);
+        'https://pythonworld.ru/uploads/books/mark-lutz-izuchaem-python-4-izd.png', FALSE, 4.7);
